2c4de2a66b7a21e3f147cd2bf8545397ec6f4690
Wolfram Klingspiegel <wolfram.klingspiegel@gmail.com>
2026-04-20 15:22:30 +0200

    Projektdateien hinzufügen.

---
+++ Übung/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+
+public class Programm {
+
+
+    public static void Main(string[] args)
+    {
+
+        string fullName = "Horst Skoff";
+        Console.WriteLine($"{{{fullName}}} ---> {GetInitials(fullName)}");
+        Console.ReadKey();  
+
+
+    }
+
+    public static string GetInitials(string fullName)
+    {
+        if (string.IsNullOrWhiteSpace(fullName))
+        {
+            throw new ArgumentException("Name cannot be null or whitespace", nameof(fullName));
+        }
+
+        string[] words = fullName.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        StringBuilder initials = new StringBuilder();
+
+        foreach (string word in words)
+        {
+            initials.Append(char.ToUpper(word[0]));
+        }
+
+        return initials.ToString();
+    }
+
+
+
+
+
+
+
+}
+
+
